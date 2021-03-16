15bf30d8ea630d7a934d770de878aa203f75ad1a
sunshaoqi <sunshq0503@163.com>
2021-03-16 13:38:53 +0800

    提交

--- ssq-gateway/src/main/java/com/ssq/gateway/SsqGatewayApplication.java
+++ ssq-gateway/src/main/java/com/ssq/gateway/SsqGatewayApplication.java
@@ -2,10 +2,11 @@ package com.ssq.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SsqGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SsqGatewayApplication.class, args);
