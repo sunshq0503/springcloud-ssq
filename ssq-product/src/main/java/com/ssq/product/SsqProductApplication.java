a0c045782f949b1878005c2a9d04803f223b39d5
sunshaoqi <sunshq0503@163.com>
2021-03-17 09:30:21 +0800

    提交

--- ssq-product/src/main/java/com/ssq/product/SsqProductApplication.java
+++ ssq-product/src/main/java/com/ssq/product/SsqProductApplication.java
@@ -2,15 +2,15 @@ package com.ssq.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
-@EnableEurekaClient
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan(basePackages="com.ssq.product.dao")
 public class SsqProductApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SsqProductApplication.class, args);
     }
-
 }
